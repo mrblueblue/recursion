0139a4d82c4f530a60c34e73ccacdace74357a74
Jonathan Huang <grokut@gmail.com>
2015-01-18 17:03:02 -0800

    reformatted using spaces

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,20 +1,26 @@
 
 
 var getElementsByClassName = function(className){
+
   var allElementsWithClass =[];
+
   recursiveTraverse(document.body, function(element) {
     if ( element.classList.contains(className) ) {
       allElementsWithClass.push(element);
     }
   });
+
   return allElementsWithClass;
 };
 
 var recursiveTraverse = function(node, iterator) {
-	iterator(node);
-	node = node.firstElementChild;
-	while (node) {
-		recursiveTraverse(node, iterator);
-		node = node.nextElementSibling;
-	}
+
+  iterator(node);
+  node = node.firstElementChild;
+
+  while (node) {
+    recursiveTraverse(node, iterator);
+    node = node.nextElementSibling;
+  }
+  
 };
