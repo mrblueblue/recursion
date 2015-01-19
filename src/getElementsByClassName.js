7e60529eaf2717cd6a1d94288a263fd29a6176b7
Jonathan Huang <grokut@gmail.com>
2015-01-18 16:58:49 -0800

    solved using a recursive traversal function and an anonymous filter function

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,10 +1,20 @@
-// If life was easy, we could just do things the easy way:
-// var getElementsByClassName = function (className) {
-//   return document.getElementsByClassName(className);
-// };
 
-// But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+
+var getElementsByClassName = function(className){
+  var allElementsWithClass =[];
+  recursiveTraverse(document.body, function(element) {
+    if ( element.classList.contains(className) ) {
+      allElementsWithClass.push(element);
+    }
+  });
+  return allElementsWithClass;
+};
+
+var recursiveTraverse = function(node, iterator) {
+	iterator(node);
+	node = node.firstElementChild;
+	while (node) {
+		recursiveTraverse(node, iterator);
+		node = node.nextElementSibling;
+	}
 };
