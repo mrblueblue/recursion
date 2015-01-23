ee8e0963d7541ba930907b60d5e351c62340b751
Jonathan Huang <grokut@gmail.com>
2015-01-23 00:22:28 -0800

    base case created

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -7,5 +7,30 @@ var parseJSON = function(json) {
   // The 'easier than the hard way' way  //
   // return eval(' new Object(' + json + ')')
 
+  if (typeof eval(json) === 'number' ||
+  	  typeof eval(json) === 'boolean' ||
+      typeof eval(json) ==='string' ||
+      typeof eval(json) === 'null') {
+
+  	return eval(json)
+  }
+
+  if ( Array.isArray(eval(json)) ) {
+    return eval(json)
+  }
+
+  else {
+    var jsonOject = {};
+
+    jsonObject[parseJSON(key)]=parseJSON(value);
+
+
+    return jsonOject;
+  }
+
+
+
   
 };
+
+console.log(typeof parseJSON('[true, 1, "hello"]'));
